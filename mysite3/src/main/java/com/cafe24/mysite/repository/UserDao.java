8aaa1e755cdf9080e713ee35145f344105f4d642
kickscar <kickscar@gmail.com>
2019-06-03 21:11:13 +0900

    쓸데없는 import

--- mysite3/src/main/java/com/cafe24/mysite/repository/UserDao.java
+++ mysite3/src/main/java/com/cafe24/mysite/repository/UserDao.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StopWatch;
 
 import com.cafe24.mysite.exception.UserDaoException;
 import com.cafe24.mysite.vo.UserVo;
